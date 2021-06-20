e4bac6d633c7ef754086c7a2082b0674f5c10ae7
rohitmendus <rohitmendus@gmail.com>
2021-06-20 17:12:14 +0530

    Added sign-in page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,58 @@
+// Sign in page start
+
+
+
+// Customising google sign in button
+function onSuccess(googleUser) {
+	console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+}
+function onFailure(error) {
+	console.log(error);
+}
+function renderButton() {
+	gapi.signin2.render('my-signin2', {
+		'scope': 'profile email',
+		'width': 320,
+		'height': 50,
+		'longtitle': true,
+		'theme': 'dark',
+		'onsuccess': onSuccess,
+		'onfailure': onFailure
+	});
+}
+
+
+
+
+// Validation of sign-in
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+	'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+  .forEach(function (form) {
+  	form.addEventListener('submit', function (event) {
+  		if (!form.checkValidity()) {
+  			event.preventDefault()
+  			event.stopPropagation()
+  		}
+
+  		form.classList.add('was-validated')
+  	}, false)
+  })
+})()
+
+
+
+
+// Sign in page end
+
+
+// Main Page Start
 var signIn = false;
 
 function check_login() {
@@ -21,3 +76,4 @@ function check_login() {
 }
 
 check_login();
+// Main Page End
